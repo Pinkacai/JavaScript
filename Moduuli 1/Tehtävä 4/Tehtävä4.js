8ccf8b273e3c7bd840ac1eba7c8e7880e0891df6
Pinkacai <eliel.j.levine@gmail.com>
2025-04-02 15:35:57 +0300

    Java + Html

--- Moduuli 1/Tehtävä 4/Tehtävä4.js
+++ Moduuli 1/Tehtävä 4/Tehtävä4.js
@@ -7,22 +7,21 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-const num = getRandomInt(4)
+const num = getRandomInt(4);
 
-let house = ('house')
+let house = ('house');
 
 if (num === 0) {
-  house = 'Ravenclaw'
+  house = 'Ravenclaw';
 } else if (num === 1) {
-  house = 'Gryffindor'
+  house = 'Gryffindor';
 } else if (num === 2) {
-  house = 'Slytherin'
+  house = 'Slytherin';
 } else if (num === 3) {
-  house = 'Hufflepuff'
+  house = 'Hufflepuff';
 }
 
-
-
 'use strict';
-document.querySelector('#target').innerHTML = name + ', you belong to ' + house + '.';
+document.querySelector('#target').innerHTML = name + ', you belong to ' +
+    house + '.';
 document.querySelector('#target').style.fontSize = 'x-large';
