415f80d211d4e69734938364bcdb90493d787010
Pinkacai <eliel.j.levine@gmail.com>
2025-04-02 14:14:13 +0300

    Java + Html

---
+++ Moduuli 1/Practise.js
@@ -0,0 +1,14 @@
+'use strict';
+console.log('I am awake');
+
+alert('BOO');
+const name = prompt('Please input your name! ');
+'use strict';
+document.querySelector('#target').innerHTML = 'Good morning, ' + name + '!';
+document.querySelector('#target').style.fontSize = 'x-large';
+
+document.querySelector('#header').innerHTML = 'Greetings';
+document.querySelector('#header').style.fontSize = 'xxx-large';
+console.log(typeof name);
+
+
