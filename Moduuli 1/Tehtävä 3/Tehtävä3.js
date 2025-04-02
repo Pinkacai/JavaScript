8dc54b1714c033b7381b86452b9e78005f98cf28
Pinkacai <eliel.j.levine@gmail.com>
2025-04-02 15:01:35 +0300

    Java + Html

--- Moduuli 1/Tehtävä 3/Tehtävä3.js
+++ Moduuli 1/Tehtävä 3/Tehtävä3.js
@@ -7,14 +7,19 @@ let age3 = parseInt(prompt('Please input one final number. '));
 
 const sum = age1 + age2 + age3;
 const mult = age1 * age2 * age3;
-const avr= sum / 3;
+const avr = sum / 3;
 'use strict';
 
-document.querySelector('#Sum').innerHTML = 'The sum of the numbers you entered is: ' + sum + '.';
+document.querySelector(
+    '#Sum').innerHTML = 'The sum of the numbers you entered is: ' + sum + '.';
 document.querySelector('#Sum').style.fontSize = 'x-large';
 
-document.querySelector('#Mult').innerHTML = 'The sum of the multiplication of the numbers you entered is: ' + mult + '.';
+document.querySelector(
+    '#Mult').innerHTML = 'The sum of the multiplication of the numbers you entered is: ' +
+    mult + '.';
 document.querySelector('#Mult').style.fontSize = 'x-large';
 
-document.querySelector('#Avr').innerHTML = 'The sum of the average of the numbers you entered is: ' + avr + '.';
+document.querySelector(
+    '#Avr').innerHTML = 'The sum of the average of the numbers you entered is: ' +
+    avr + '.';
 document.querySelector('#Avr').style.fontSize = 'x-large';
