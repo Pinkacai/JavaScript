a46b756ed3963e59895f14e5531001d43a2394d2
Pinkacai <eliel.j.levine@gmail.com>
2025-04-02 15:25:37 +0300

    Java + Html

---
+++ Moduuli 1/Tehtävä 4/Tehtävä4.js
@@ -0,0 +1,28 @@
+'use strict';
+console.log('I am awake');
+
+const name = prompt('Please input your name! ');
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const num = getRandomInt(4)
+
+let house = ('house')
+
+if (num === 0) {
+  house = 'Ravenclaw'
+} else if (num === 1) {
+  house = 'Gryffindor'
+} else if (num === 2) {
+  house = 'Slytherin'
+} else if (num === 3) {
+  house = 'Hufflepuff'
+}
+
+
+
+'use strict';
+document.querySelector('#target').innerHTML = name + ', you belong to ' + house + '.';
+document.querySelector('#target').style.fontSize = 'x-large';
