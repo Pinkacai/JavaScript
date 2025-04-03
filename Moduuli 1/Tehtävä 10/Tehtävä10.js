64e1a1e15abb57fc1f6aae085efb1caeb3365312
Pinkacai <eliel.j.levine@gmail.com>
2025-04-03 15:22:06 +0300

    Java + Html

--- Moduuli 1/Tehtävä 10/Tehtävä10.js
+++ Moduuli 1/Tehtävä 10/Tehtävä10.js
@@ -1,24 +1,33 @@
 'use strict';
 
-const throws = parseInt(prompt("How many dice would you like to throw?"))
-const sum_num = parseInt(prompt("Which number are you interested in finding out the probability of the dice throwing said number?"))
-
+const throws = parseInt(prompt('How many dice would you like to throw?'));
+const sum_num = parseInt(prompt(
+    'Which sum are you interested in finding out the probability of the dice throwing?'));
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max) + 1;
 }
 
-const num = getRandomInt(6);
+let prob = 0;
+let repetitions = 100000
+let succesfulthrows = 0;
 
-
-let prob = 0
-for (let start = 1; start < throws; start++) {
-  if (num === sum_num){
-    prob++
+while (prob < repetitions) {
+  let sum = 0;
+  for (let start = 0; start < throws; start++) {
+    const num = getRandomInt(6);
+    sum += num;
+    }
+  if (sum === sum_num) {
+      succesfulthrows++;
   }
+  prob++;
 }
 
-const prob_calc = (prob * 100)/throws
+const prob_calc = (succesfulthrows * 100.0) / repetitions;
 
+document.querySelector('#sum').innerHTML = 'The probability of throwing a ' +
+    sum_num + ' with ' + throws + ' throws, is ' + prob_calc;
+document.querySelector('#sum').style.fontSize = 'xxx-large';
 
-document.querySelector("#sum").innerHTML = "The probability of throwing a " + sum_num + " with " + throws + " throws, is " + prob_calc
+document.querySelector("#sum").innerHTML += " as" + succesfulthrows
