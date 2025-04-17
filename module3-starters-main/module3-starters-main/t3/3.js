996b581c4358fb15ab5f86d723dfdaa7641c0353
Pinkacai <eliel.j.levine@gmail.com>
2025-04-17 13:44:57 +0300

    So

--- module3-starters-main/module3-starters-main/t3/3.js
+++ module3-starters-main/module3-starters-main/t3/3.js
@@ -1,2 +1,9 @@
 'use strict';
 const names = ['John', 'Paul', 'Jones'];
+const list = document.querySelector("#target")
+
+let named = ''
+for (let i of names) {
+  named += `<li>${i}</li>`
+}
+list.innerHTML = named
