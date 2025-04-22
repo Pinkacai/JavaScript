c53f439524706265e33924428ddd0da2a674e358
Pinkacai <eliel.j.levine@gmail.com>
2025-04-22 12:38:33 +0300

    Java + Html

---
+++ Moduuli 4/practise.js
@@ -0,0 +1,12 @@
+//'use strict'
+
+//let name;
+
+//document.getElementById("button").onmouseenter = function(){
+//name = document.getElementById("value").value
+//document.getElementById("start").textContent = `Hello ${name}!`
+//}
+
+//document.querySelector("body").style.fontSize = "xx-large"
+//document.querySelector("button").style.fontSize = "xx-large"
+//document.querySelector("input").style.fontSize = "xx-large"
